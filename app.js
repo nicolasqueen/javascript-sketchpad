62823959d13e1ab786a476e897b339bfcdc35aa3
Nick Queen <nicolasqueen@gmail.com>
2015-11-01 12:24:50 -0500

    Grid created and color change working

---
+++ app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	for (var x = 0; x < 64; x++) {
+		for (var i=0; i<64; i++){
+			var grid = $('<div class="gridSquares"></div>');
+			grid.appendTo('#container');
+		}
+	}
+
+	$(".gridSquares").hover(function() {
+		/* Stuff to do when the mouse enters the element */
+		$(this).addClass('gridChanged')
+	}, function() {
+		/* Stuff to do when the mouse leaves the element */
+	});
+});
